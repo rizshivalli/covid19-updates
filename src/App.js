678527138ed697b19fe31c82eb920848629fa1e1
Rizwan Shivalli <rizwan.a@simplificommerce.com>
2020-03-22 11:14:30 +0530

    improved code quality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Container } from "@material-ui/core";
+import {
+  Grid,
+  Container,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  CircularProgress
+} from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import axios from "axios";
 import "./index.css";
 import countryCode from "./code";
 
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    }
+  })
+);
 export default function App() {
   const [countryData, setCountryData] = useState([]);
   const [worldData, setWorldData] = useState({});
   const [date, setDate] = useState(1584724653093);
   const [search, setSearch] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
+  const classes = useStyles();
+  const styles = {
+    flexEnd: {
+      display: "flex",
+      alignItems: "flex-end"
+    },
+    recoveryColor: { color: "#27ae60" },
+    totalColor: { color: "#f1c40f" },
+    deathRateColor: { color: "#e74c3c" },
+    flagStyles: { margin: "3px", marginRight: "20px" }
+  };
 
   useEffect(() => {
     axios.get("https://corona.lmao.ninja/countries").then(res => {
       setCountryData([...res.data]);
+      console.log({ res });
     });
 
     axios.get("https://corona.lmao.ninja/all").then(res => {
@@ -22,14 +55,19 @@ export default function App() {
     });
   }, []);
 
-  const handleChnage = e => {
+  const handleChange = e => {
     setSearch(e.target.value);
   };
 
   let data = countryData;
-  var updatedDate = new Date(date)
-  var day = updatedDate.getDate() + '-' + (updatedDate.getMonth() + 1) + '-' + updatedDate.getFullYear();
-  var time = updatedDate.getHours() + ' : ' + updatedDate.getMinutes();
+  var updatedDate = new Date(date);
+  var day =
+    updatedDate.getDate() +
+    "-" +
+    (updatedDate.getMonth() + 1) +
+    "-" +
+    updatedDate.getFullYear();
+  var time = updatedDate.getHours() + " : " + updatedDate.getMinutes();
 
   if (search.length > 0) {
     let searchResult = countryData.filter(item =>
@@ -41,37 +79,41 @@ export default function App() {
   }
 
   return (
-    <Container
-      maxWidth="lg"
-      className="app-container"
-    >
+    <Container maxWidth="lg" className="app-container">
       {countryData.length > 0 ? (
         <Grid container>
-          <Grid item md={4} xs={12} style={{marginRight:"10px"}}>
+          <AppBar
+            position="static"
+            color="white"
+            style={{ marginBottom: "1%" }}
+          >
+            <Toolbar>
+              <Typography variant="h6" className={classes.root}>
+                corona.org.in
+              </Typography>
+              <IconButton
+                href="https://github.com/rizshivalli/covid19-updates"
+                target="_blank"
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+
+          <Grid item md={4} xs={12} style={{ marginRight: "10px" }}>
             <h1 className="heading-one">COVID19 </h1>
             <h1 className="heading-two">UPDATES </h1>
           </Grid>
 
-          <Grid
-            item
-            md={7}
-            xs={12}
-            style={{
-              display: "flex",
-              alignItems: "flex-end"
-            }}
-          >
+          <Grid item md={7} xs={12} style={styles.flexEnd}>
             <Grid container>
               <Grid item md={3} xs={4}>
                 <div className="world-data-item">
                   <div>
-                    <p
-                      className="caption"
-                      style={{ color: "#f1c40f" }}
-                    >
+                    <p className="caption" style={styles.totalColor}>
                       Total Cases
                     </p>
-                    <p className="title-large" style={{ color: "#f1c40f" }}>
+                    <p className="title-large" style={styles.totalColor}>
                       {worldData.cases}
                     </p>
                   </div>
@@ -80,18 +122,13 @@ export default function App() {
               <Grid item md={3} xs={4}>
                 <div className="world-data-item">
                   <div>
-                    <p
-                      className="caption"
-                      style={{ color: "#27ae60" }}
-                    >
+                    <p className="caption" style={styles.recoveryColor}>
                       Recovery Rate
                     </p>
-                    <p className="title-large" style={{ color: "#27ae60" }}>
+                    <p className="title-large" style={styles.recoveryColor}>
                       {(
                         Math.round(
-                          ((100 * worldData.recovered) /
-                            worldData.cases) *
-                            100
+                          ((100 * worldData.recovered) / worldData.cases) * 100
                         ) / 100
                       ).toFixed(2)}{" "}
                       %
@@ -103,16 +140,12 @@ export default function App() {
               <Grid item md={3} xs={4}>
                 <div className="world-data-item">
                   <div>
-                    <p
-                      className="caption"
-                      style={{ color: "#e74c3c" }}
-                    >
+                    <p className="caption" style={styles.deathRateColor}>
                       Death Rate
                     </p>
-                    <p className="title-large" style={{ color: "#e74c3c" }}>
+                    <p className="title-large" style={styles.deathRateColor}>
                       {Math.round(
-                        ((100 * worldData.deaths) / worldData.cases) *
-                          100
+                        ((100 * worldData.deaths) / worldData.cases) * 100
                       ) / 100}{" "}
                       %
                     </p>
@@ -126,7 +159,7 @@ export default function App() {
             <input
               className="input-box"
               placeholder="Search Country...."
-              onChange={handleChnage}
+              onChange={handleChange}
             />
           </Grid>
 
@@ -155,7 +188,7 @@ export default function App() {
           </Grid>
 
           {data.length > 0 ? (
-            data.map(item => {
+            data.map((item, index) => {
               let recoveredRate = (
                 Math.round(((100 * item.recovered) / item.cases) * 100) / 100
               ).toFixed(2);
@@ -171,32 +204,33 @@ export default function App() {
               }/shiny/64.png`;
 
               return (
-                <Grid item md={12}>
+                <Grid key={index} item md={12}>
                   <Grid container>
                     <Grid item xs={12} md={4} className="flex-start">
                       <img
                         src={image}
                         width="50px"
                         alt="flag"
-                        style={{ margin: "3px", marginRight: "20px" }}
+                        style={styles.flagStyles}
                       />
-                      <p className="titile">{item.country}</p>
+                      <p className="title">{item.country}</p>
                     </Grid>
                     <Grid item xs={3} md={2} className="flex-start">
-                      <div className="country-data-item" style={{backgroundColor:"#f1c40f"}} >
+                      <div
+                        className="country-data-item"
+                        style={{ backgroundColor: "#f1c40f" }}
+                      >
                         <div>
                           <p className="caption text-hide">Cases</p>
-                          <p className="titile">{item.cases}</p>
+                          <p className="title">{item.cases}</p>
                         </div>
                       </div>
                     </Grid>
                     <Grid item xs={3} md={2} className="flex-start">
                       <div className="country-data-item">
                         <div>
-                          <p className="caption text-hide">
-                            Recovered
-                          </p>
-                          <p className="titile">{item.recovered}</p>{" "}
+                          <p className="caption text-hide">Recovered</p>
+                          <p className="title">{item.recovered}</p>{" "}
                         </div>
                       </div>
                     </Grid>
@@ -206,10 +240,8 @@ export default function App() {
                         style={{ backgroundColor: "#27ae60" }}
                       >
                         <div>
-                          <p className="caption text-hide">
-                            Recovery
-                          </p>
-                          <p className="titile">{recoveredRate} %</p>{" "}
+                          <p className="caption text-hide">Recovery</p>
+                          <p className="title">{recoveredRate} %</p>{" "}
                         </div>
                       </div>
                     </Grid>
@@ -220,7 +252,7 @@ export default function App() {
                       >
                         <div>
                           <p className="caption text-hide">Death</p>
-                          <p className="titile">{deathsRate} %</p>
+                          <p className="title">{deathsRate} %</p>
                         </div>
                       </div>
                     </Grid>
@@ -232,33 +264,31 @@ export default function App() {
             <p className="footer-text">No results</p>
           )}
 
-          <Grid
-            item
-            xs={12}
-            md={12}
-            className="footer"
-          >
+          <Grid item xs={12} md={12} className="footer">
             <div>
-            <p className="footer-text">
-            Source from  <a target="blank" href="https://www.worldometers.info">worldometers.info</a>
-            </p>
-            <p className="footer-text">
-            API from <a target="blank" href="https://www.programmableweb.com/">programmableweb.com</a>
-            </p>
-            <p className="footer-text">
-          Last update at
-            {"   "}
-             
-            {time}
-            {"   "} 
-             &
-            {"   "}
-                
-            {day} 
-            </p>
-            <p className="footer-text">
-            Created  by <a target="blank" href="http://linkedin.com/in/muqsithck" >  Abdul Muqsith</a>
-            </p>
+              <p className="footer-text">
+                Source from{" "}
+                <a target="blank" href="https://www.worldometers.info">
+                  worldometers.info
+                </a>
+              </p>
+              <p className="footer-text">
+                API from{" "}
+                <a target="blank" href="https://www.programmableweb.com/">
+                  programmableweb.com
+                </a>
+              </p>
+              <p className="footer-text">
+                Created by{" "}
+                <a target="blank" href="http://linkedin.com/in/muqsithck">
+                  {" "}
+                  Abdul Muqsith
+                </a>{" "}
+                and{" "}
+                <a target="blank" href="https://rizwanshivalli.me">
+                  Rizwan Shivalli
+                </a>
+              </p>
             </div>
           </Grid>
 
@@ -266,7 +296,7 @@ export default function App() {
         </Grid>
       ) : (
         <Grid className="loading-screen">
-          <img src={require("./assets/loading.gif")} alt="gif" width="40px" />
+          <CircularProgress size={100} thickness={4} />
         </Grid>
       )}
     </Container>
