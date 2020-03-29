938091a8d8579bff79a772fac30df1521f813fbf
Rizwan Shivalli <rizwan.a@simplificommerce.com>
2020-03-29 15:30:45 +0530

    bootstrap input added

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import CovidTypography from "./CovidTypography";
 import GithubIcon from "./GithubIcon";
 import Footer from "./Footer";
 import TabPanel from "./TabPanel";
+import BootstrapInput from "./BootstrapInput";
 
-export { CovidTypography, GithubIcon, Footer, TabPanel };
+export { CovidTypography, GithubIcon, Footer, TabPanel, BootstrapInput };
